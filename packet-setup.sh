04d05d735b16c6e40a712fcee1d311b72ab22b95
Franziskus Kiefer <franziskuskiefer@gmail.com>
2017-02-13 11:58:25 +0100

    systemd script for docker-worker

--- packet-setup.sh
+++ packet-setup.sh
@@ -8,25 +8,18 @@ chown -R ci:ci /home/ci
 # update and set-up docker
 apt-get update
 apt-get upgrade -y
-apt-get install -y --no-install-recommends docker.io gcc g++ make
+apt-get install -y --no-install-recommends docker.io gcc g++ make unzip git
 gpasswd -a ci docker
 systemctl start docker
 
+# make docker-worker work later
+touch /etc/docker-worker-priv.pem
+
 # set-up ci stuff
-su ci
-cd
+su -c "./setup-ci.sh 1" -s /bin/bash ci
 
-# set-up docker-worker things
-mkdir hello-world-aarch64 && cd hello-world-aarch64
-echo "FROM aarch64/hello-world" > Dockerfile
-docker build -t taskcluster/livelog:v4 .
-cd ..
+# install nodejs
+cd /home/ci/node && make install
 
-# build 0.12.8 from source
-wget https://franziskuskiefer.de/data/node012.zip
-unzip node012.zip
-cd node-v0.12.8
-make -j100
-exit && cd /home/ci/node-v0.12.8 && make install
-su ci && cd /home/ci
+su -c "./setup-ci.sh 2" -s /bin/bash ci
 
